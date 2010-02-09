b73eae64ab1d3950b0a977eafe2d7c4bdff2e3c0
Gabriel Pettier <gabriel.pettier@alterway.fr>
2010-02-09 18:24:20 +0100

    template app_list de base fonctionel

--- aws.demos/awsdemos/views.py
+++ aws.demos/awsdemos/views.py
@@ -1,5 +1,25 @@
+import webob
+from repoze.bfg.chameleon_zpt import get_template
+from repoze.bfg.chameleon_zpt import render_template_to_response
+
 def my_view(request):
     return {'project':'aws.demos'}
 
-def app_list(request):
-    return repr(request)
+def app_list(context, request):
+    demos = {}
+    last = ""
+    file = open("awsdemos/demos.cfg")
+    for line in file.readlines():
+        line = line[:-1]
+        if line[:4] == 4*' ':
+            demos[last][line[4:].split(':')[0]] = line.split(':')[1]
+        else:
+            demos[line]= {}
+            last = line
+    file.close()
+    return render_template_to_response(
+                "templates/app_list.pt",
+                context=context,
+                request=request,
+                demos=demos
+                )
