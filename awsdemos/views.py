996c6ef8d41bebfae38053676af50d34e41727f4
Gabriel Pettier <gabriel.pettier@alterway.fr>
2010-03-01 18:41:40 +0100

    buttons

--- awsdemos/views.py
+++ awsdemos/views.py
@@ -3,6 +3,7 @@ from repoze.bfg.chameleon_zpt import get_template
 from repoze.bfg.chameleon_zpt import render_template_to_response
 from repoze.bfg.exceptions import NotFound
 import subprocess
+import os
 import time
 import webob
 from repoze.bfg.security import (
@@ -20,24 +21,9 @@ def load_app_list():
     return a dict containing all apps and their respective commands defined in
     config file.
     """
-    conf = ConfigParser()
-    conf.readfp(open('supervisor.cfg'))
-    demos = {}
-    #last = ""
-    #file = open("awsdemos/demos.cfg")
-    #for line in file.readlines():
-        #line = line[:-1]
-        #if line[:4] == 4*' ':
-            #demos[last][line[4:].split(':')[0]] = line.split(':')[1]
-        #else:
-            #demos[line]= {}
-            #last = line
-    #file.close()
-    for k in conf.sections():
-        demos[k] = {}
-        for i in conf.items(k):
-            demos[k][i[0]] = i[1]
-    print demos
+    demos = []
+    for file in (i for i in os.listdir('scripts') if 'demo_' in i and '.sh' in i):
+        demos.append(file[5:-3])
     return demos
 
 
@@ -46,11 +32,10 @@ def app_list(request):
     return the main page, with applications list, and actions.
     """
     return render_template_to_response(
-                "templates/app_list.pt",
-                request=request,
-                demos=load_app_list()
-                )
-
+        "templates/app_list.pt",
+        request=request,
+        demos=load_app_list()
+        )
 
 def LOG(string):
     print 'LOG: '+string
@@ -66,16 +51,13 @@ def action(request):
     finished.
     FIXME: should verify if the user has access to the command.
     """
-    app_list = load_app_list()
-    try:
-        app = request.params['app']
-        action = request.params['action']
-        command = app_list[app][action]
-    except KeyError:
-        raise NotFound
-    LOG(command)
-    process = subprocess.Popen(command, shell=True)
-    process.wait()
-    return webob.Response(str("action finished"))
+    if request.params['app'] in load_app_list():
+        command = "demo_"+request.params['app']+".sh"
+        LOG(command)
+        process = subprocess.Popen(command, shell=True)
+        stdout, stderr = process.communicate()
+        return webob.Response(str("action finished"+str(stdout))+str(stderr))
+    else:
+        return webob.Response(str("unknown action"))
 
 
