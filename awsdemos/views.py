218c88ce531027ef973bce459a249c588f514138
Gabriel Pettier <gabriel.pettier@alterway.fr>
2010-03-24 14:20:59 +0100

    fix empty demo list

--- awsdemos/views.py
+++ awsdemos/views.py
@@ -171,7 +171,7 @@ def demos_list():
             config.get('program:'+d, 'port'),
             config.get('program:'+d, 'comment')
         )
-        for d in os.listdir('demos') if os.path.isfile(d)
+        for d in os.listdir('demos') if os.path.isfile('demos'+os.sep+d)
         ]
 
 def view_demos_list(request, message=None):
