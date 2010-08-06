928b14f11b0121a585ba13a48e274c892d6cc27d
Christophe Combelles <ccomb@free.fr>
2010-08-07 00:37:42 +0200

    Added BLUEBREAM!!

---
+++ scripts/demo_bluebream.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+# PARAMS:NAME,COMMENT,TEST
+
+set -e # explicit fail on errors
+
+# create a virtualenv
+virtualenv --no-site-packages --distribute sandbox
+
+# install the required packages
+sandbox/bin/pip install --download-cache=$HOME/eggs bluebream==1.0b2
+
+# create a bfg project
+sandbox/bin/paster create --no-interactive -t bluebream bb
+cd bb
+
+# change the port used by default
+sed -i "s/^port = .*/port = $PORT/" "deploy.ini"
+
+# bootstrap and buildout
+../sandbox/bin/python bootstrap.py
+./bin/buildout
+
+cat > ../start.sh << EOF
+cd bb
+bin/paster serve deploy.ini
+EOF
+
