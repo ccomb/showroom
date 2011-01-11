e390247a7ca76fdb026f23e29e8a1378f918911d
Christophe Combelles <ccomb@free.fr>
2011-01-11 18:06:28 +0100

    first draft for SugarCRM

---
+++ scripts/demo_SugarCRM.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# PARAMS: name
+# sudo aptitude install php5-cli apache2
+
+# download sugar
+version=6.1.0
+
+wget http://www.sugarforge.org/frs/download.php/7567/SugarCE-$version.zip
+unzip SugarCE-$version.zip
+mv SugarCE-Full-$version sugar
+
+# create the Apache config
+cat > apache2.conf << EOF
+Listen $PORT
+NameVirtualHost *:$PORT
+<VirtualHost *:$PORT>
+ServerName localhost
+DocumentRoot $PWD/sugar
+#ErrorLog var/error.log
+</VirtualHost>
+EOF
+
+# initialize MySQL
+mkdir mysql
+mysql_install_db --no-defaults --datadir=$PWD/mysql/
+
+# start mysql temporarily
+/usr/sbin/mysqld --no-defaults --socket=$PWD/mysql/mysqld.sock --datadir=$PWD/mysql/ --log-error=$PWD/mysql/mysql-error.log --port=$((PORT+1000)) &
+
+# wait for mysql to be started
+echo "Waiting for mysql to start..."
+mysqladmin --socket=$PWD/mysql/mysqld.sock --user=root status
+while [ $? -ne 0 ]; do
+    sleep 0.5; echo "Waiting for mysql to start..."
+    mysqladmin --socket=$PWD/mysql/mysqld.sock --user=root status
+done
+
+# create a database
+mysqladmin --socket=$PWD/mysql/mysqld.sock --user=root create sugar
+
+# create a user with all privileges on the database
+echo "CREATE USER 'sugar'@'localhost' IDENTIFIED BY 'sugar';" > mysql.tmp
+echo "GRANT ALL ON sugar.* TO 'sugar'@'localhost';" >> mysql.tmp
+echo "FLUSH PRIVILEGES;" >> mysql.tmp
+mysql --socket=$PWD/mysql/mysqld.sock --user=root mysql < mysql.tmp
+rm mysql.tmp
+
+# stop mysql
+mysqladmin --socket=$PWD/mysql/mysqld.sock --user=root shutdown
+
+# create a startup script
+cat > start.sh << EOF
+exec /usr/sbin/mysqld --no-defaults --socket=$PWD/mysql/mysqld.sock --datadir=$PWD/mysql/ --log-error=$PWD/mysql/mysql-error.log --port=$((PORT+1000))
+EOF
+
+
