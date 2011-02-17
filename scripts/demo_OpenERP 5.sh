6eb5bfa6507c8be15c13b87af7a3b461c9ba4398
Christophe Combelles <ccomb@free.fr>
2011-02-17 15:58:57 +0100

    made openerp versions configurable

--- scripts/demo_OpenERP 5.sh
+++ scripts/demo_OpenERP 5.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# PARAMS:name
+# PARAMS:name, version=5.0.15
 set -e
 
 
@@ -11,8 +11,6 @@ virtualenv --no-site-packages --distribute sandbox
 sandbox/bin/easy_install http://downloads.egenix.com/python/egenix-mx-base-3.1.2.tar.gz
 sandbox/bin/pip install http://download.gna.org/pychart/PyChart-1.39.tar.gz
 sandbox/bin/pip install --download-cache=$HOME/eggs psycopg2==2.2.2 reportlab==2.4 pydot==1.0.2 lxml==2.2.8 pytz==2010k PIL==1.1.7
-
-version=5.0.15
 
 # download and install the server
 wget http://openerp.com/download/stable/source/openerp-server-$version.tar.gz
--- scripts/demo_OpenERP 6.sh
+++ scripts/demo_OpenERP 6.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# PARAMS:name
+# PARAMS:name, version=6.0.1
 set -e
 
 
@@ -12,17 +12,15 @@ sandbox/bin/easy_install http://downloads.egenix.com/python/egenix-mx-base-3.1.2
 sandbox/bin/pip install http://download.gna.org/pychart/PyChart-1.39.tar.gz
 sandbox/bin/pip install --download-cache=$HOME/eggs psycopg2==2.2.2 reportlab==2.4 pydot==1.0.2 lxml==2.2.8 pytz==2010k PIL==1.1.7 PyYAML==3.09
 
-version=6.0.0-rc2
-
 # download and install the server
-wget http://openerp.com/download/unstable/source/openerp-server-$version.tar.gz
+wget http://openerp.com/download/stable/source/openerp-server-$version.tar.gz
 tar xzf openerp-server-$version.tar.gz
 cd openerp-server-$version
 ../sandbox/bin/python setup.py install
 cd ..
 
 # download and install the web interface
-wget http://openerp.com/download/unstable/source/openerp-web-$version.tar.gz
+wget http://openerp.com/download/stable/source/openerp-web-$version.tar.gz
 tar xzf openerp-web-$version.tar.gz
 cd openerp-web-$version
 ../sandbox/bin/python setup.py install
