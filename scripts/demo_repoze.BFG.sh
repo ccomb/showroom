63e89da9cf1ffb2774152b0b7151383f95355d48
Christophe Combelles <ccomb@free.fr>
2012-05-21 17:00:09 +0200

    updated the bfg demo

--- scripts/demo_repoze.BFG.sh
+++ scripts/demo_repoze.BFG.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # PARAMS:name
 
+function first_install {
 # create a virtualenv
 virtualenv --no-site-packages --distribute sandbox
 
 # install the required packages
-sandbox/bin/pip install -i http://dist.repoze.org/bfg/current/simple repoze.bfg==1.3
+sandbox/bin/pip install -i http://dist.repoze.org/bfg/current/simple --download-cache=$HOME/eggs repoze.bfg==1.3
 
 # create a bfg project
 sandbox/bin/paster create -t bfg_starter bfg
@@ -17,7 +18,17 @@ sed -i "s/port = 6543/port = $PORT/" "bfg.ini"
 # install our new application in the virtualenv
 ../sandbox/bin/python setup.py develop
 
-cat > ../start.sh << EOF
+cd ..
+# relocate the virtualenv
+virtualenv --no-site-packages --distribute sandbox --relocatable
+
+cat > start.sh << EOF
 exec sandbox/bin/paster serve bfg/bfg.ini
 EOF
 
+}
+
+function reconfigure_clone {
+cd bfg
+sed -i "s/^port =.*/port = $PORT/" "bfg.ini"
+}
