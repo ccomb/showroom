9b28b3c19ccb78913aa42daf55047825abfb386f
Christophe Combelles <ccomb@free.fr>
2013-03-24 00:16:21 +0100

    workflow, style, states

--- addons/showroom/application/model.py
+++ addons/showroom/application/model.py
@@ -10,6 +10,9 @@ class Application(osv.Model):
         'name': fields.char(
             'Name',
             size=64,
+            readonly=True,
+            required=True,
+            states={'draft': [('readonly', False)]},
             help='Name of the application'),
         'host_id': fields.many2one(
             'showroom.server',
@@ -26,7 +29,19 @@ class Application(osv.Model):
         'template_id': fields.many2one(
             'showroom.template',
             'Type',
+            readonly=True,
+            required=True,
+            states={'draft': [('readonly', False)]},
             help='Type of app'),
+        'state': fields.selection(
+            [('draft', 'Draft'),
+             ('deploy', 'Deploying'),
+             ('stopped', 'Stopped'),
+             ('running', 'Running'),
+             ],
+            'State',
+            required=True,
+            help='Current state of the application'),
     }
 
     def _choose_server(self, cr, uid, ids, context=None):
@@ -44,6 +59,32 @@ class Application(osv.Model):
                 'No more available server now. Please retry later.')
         return chosen_server
 
+    def draft(self, cr, uid, ids, context=None):
+        """ Return the application to draft
+        """
+        self.write(cr, uid, ids, {'state': 'draft'}, context)
+
+    def deploy(self, cr, uid, ids, context=None):
+        """ Deploy the application
+        """
+        self.write(cr, uid, ids, {'state': 'deploy'}, context)
+
+    def start(self, cr, uid, ids, context=None):
+        """ Start the application
+        """
+        self.write(cr, uid, ids, {'state': 'running'}, context)
+
+    def stop(self, cr, uid, ids, context=None):
+        """ Stop the application
+        """
+        self.write(cr, uid, ids, {'state': 'stopped'}, context)
+
+    def destroy(self, cr, uid, ids, context=None):
+        """ destroy the application
+        """
+        self.write(cr, uid, ids, {'state': 'draft'}, context)
+
     _defaults = {
         'user_id': lambda self, cr, uid, context: uid,
+        'state': 'draft',
     }
