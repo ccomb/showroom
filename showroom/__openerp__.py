1f9bc5fef2156adbb2263028f1a6dd81021bec30
Christophe Combelles <ccomb@free.fr>
2013-03-23 14:20:46 +0100

    module template

---
+++ showroom/__openerp__.py
@@ -0,0 +1,43 @@
+#==============================================================================
+#                                                                             =
+#    showroom module for OpenERP, showroom
+#    Copyright (C) 2013 Gorfou (<http://http://gorfou.fr>) 
+#                         Christophe Combelles <ccomb@gorfou.fr>
+#                                                                             =
+#    This file is a part of showroom
+#                                                                             =
+#    showroom is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License v3 or later
+#    as published by the Free Software Foundation, either version 3 of the 
+#    License, or (at your option) any later version.
+#                                                                             =
+#    showroom is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License v3 or later for more details.
+#                                                                             =
+#    You should have received a copy of the GNU Affero General Public License
+#    v3 or later along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#                                                                             =
+#==============================================================================
+{
+    'name': 'showroom',
+    'version': '0.1',
+    'category': 'showroom',
+    'description': """
+    showroom
+    """,
+    'author': 'Christophe Combelles',
+    'website': 'http://gorfou.fr',
+    'depends': ['base'],
+    'init_xml': [],
+    'update_xml': [],
+    'test': [],
+    'demo_xml': [],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    'license': 'AGPL-3',
+}
+
